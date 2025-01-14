f64bb1cd258f0c82141b24ce0536d0ae2e25e46f
mhmdcs <37247525+mhmdcs@users.noreply.github.com>
2025-01-14 21:40:04 +0300

    initial commit

---
+++ phase_2/investor_bulletin/api/routes.py
@@ -0,0 +1,5 @@
+# from api.controllers.market_controllers import router as MarketRouter
+
+def init_routes(app):
+    # app.include_router(MarketRouter, prefix="/market", tags=["Market"])
+    # return app
