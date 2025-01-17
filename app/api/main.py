53e4ed29dbc5b3d284a12815b89453724590ce71
mhmdcs <37247525+mhmdcs@users.noreply.github.com>
2025-01-18 00:16:08 +0300

    phase 1 - set up dockerfile

--- app/api/main.py
+++ app/api/main.py
@@ -1,5 +1,8 @@
 from uvicorn import run
 from fastapi import FastAPI
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from api.routes import init_routes
 
 app = init_routes(FastAPI())
