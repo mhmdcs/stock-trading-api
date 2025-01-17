e7276605e13d67f6117d84d4def93c75ba1b26f4
mhmdcs <37247525+mhmdcs@users.noreply.github.com>
2025-01-17 18:37:41 +0300

    phase 1 - set up root endpoint

--- app/api/main.py
+++ app/api/main.py
@@ -1,9 +1,12 @@
-# from uvicorn import run
-# from fastapi import FastAPI
+from uvicorn import run
+from fastapi import FastAPI
+from api.routes import init_routes
 
-# from api.routes import init_routes
+app = init_routes(FastAPI())
 
-# app = init_routes(FastAPI())
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the Investor Bulletin API 📈", "version": "1.0.0", "documentation_url": "/docs"}
 
-# if __name__ == "__main__":
-    # run("api.main:app")
+if __name__ == "__main__":
+    run("api.main:app")
--- app/api/routes.py
+++ app/api/routes.py
@@ -2,4 +2,4 @@
 
 def init_routes(app):
     # app.include_router(MarketRouter, prefix="/market-prices", tags=["Market"])
-    # return app
+    return app
